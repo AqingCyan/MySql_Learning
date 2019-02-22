29a7e8040dc52788ecd2e4ecae2260569593b1c8
AqingCyan <xuekaiqi@creatshare.com>
2019-02-22 17:19:04 +0800

    :chart_with_upwards_trend:学会了新东西

---
+++ 01_查询/流程控制函数.sql
@@ -0,0 +1,49 @@
+USE myemployees;
+# 流程控制函数
+# IF函数：实现if else效果
+SELECT IF(10 > 5, '大', '小');
+
+# 案例：查询是否有奖金
+SELECT CONCAT(last_name, first_name),
+       commission_pct,
+       IF(commission_pct IS NULL, '没奖金', '有奖金') AS '备注'
+FROM employees;
+
+# CASE函数：类似于switch case的效果
+# 案例：查询员工的工资，要求：
+# 部门号=30，显示的工资为1.1倍
+# 部门号=40，显示的工资为1.2倍
+# 部门号=50，显示的工资为1.3倍
+# 其他部门，显示工资为原工资
+/*
+格式：
+CASE 表达式或条件
+WHEN 常量1 THEN 要执行的语句1或要显示的值1
+WHEN 常量2 THEN 要执行的语句2或要显示的值2
+ELSE 要执行的默认语句或要显示的默认值
+END
+*/
+SELECT salary   '原始工资',
+       department_id,
+       CASE department_id
+         WHEN 30 THEN salary * 1.1
+         WHEN 40 THEN salary * 1.2
+         WHEN 50 THEN salary * 1.3
+         ELSE salary
+         END AS '新工资'
+FROM employees;
+
+# 案例：查询员工的工资情况、
+# 如果工资大于20000，显示A级别
+# 如果工资大于15000，显示B级别
+# 如果工资大于10000，显示C级别
+# 否则，显示D级别
+SELECT CONCAT(last_name, first_name) AS 'name',
+       salary,
+       CASE
+         WHEN salary > 20000 THEN 'A'
+         WHEN salary BETWEEN 15000 AND 20000 THEN 'B'
+         WHEN salary < 15000 THEN 'C'
+         ELSE 'D'
+         END AS '级别'
+FROM employees;
